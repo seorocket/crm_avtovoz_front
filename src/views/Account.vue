bc2b9914865d2d494373fb65934743bf0bba4c5c
Vladislav Safronov <vladislav.safronov@dentsu.ru>
2022-05-09 11:06:00 +0300

    fix

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -2,16 +2,22 @@
   div
     div.row
       div.q-pa-md
-        div Имя: {{user.first_name}}
-        div Фамилия: {{user.last_name}}
-        div Телефон: {{user.phone}}
-        div Телеграм: {{user.telegram}}
+        div(class="q-pa-md q-gutter-sm")
+          div Имя: {{user.first_name}}
+          div Фамилия: {{user.last_name}}
+          div Телефон: {{user.phone}}
+          div Телеграм: {{user.telegram}}
+          q-btn(
+            flat
+            label="Изменить"
+            color="primary"
+            v-on:click="popup.edit_popup = true"
+            )
         q-btn(
-          flat
-          label="Изменить"
-          color="primary"
-          v-on:click="popup.edit_popup = true"
-          )
+          :color="user.working ? 'secondary' : 'red'" 
+          @click="changeWorkStatus()" 
+          :label="user.working ? 'Работаю' : 'Не работаю'" 
+        )
     q-dialog(
       v-model="popup.edit_popup"
       persistent
@@ -65,7 +71,8 @@ export default {
         first_name: '',
         last_name: '',
         phone: '',
-        telegram: ''
+        telegram: '',
+        working: true
       },
       popup: {
         edit_popup: false
@@ -91,6 +98,14 @@ export default {
         message: message,
         position: position,
         timeout: 3000
+      })
+    },
+    changeWorkStatus () {
+      const vm = this
+      vm.user.working = !vm.user.working
+      vm.Axios.post(`/api/operator/${vm.user.pk}/change_work_status/`, {'working': vm.user.working}).then(response => {
+        vm.user = response.data
+        vm.showNotify('top-right', 'Данные обновлены', 'positive')
       })
     },
     updateData () {
